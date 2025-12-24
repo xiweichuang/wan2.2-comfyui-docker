7e45f41a8907813bd9e8c3894f54555db4d2b3e8
xiweichuang <58962460+xiweichuang@users.noreply.github.com>
2025-12-24 19:25:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,7 +66,7 @@ pip install --no-cache-dir --force-reinstall \
 torch torchvision torchaudio \
 pillow huggingface_hub accelerate optimum av \
 "transformers>=4.57.1" qwen-vl-utils \
-opencv-python-headless==4.12.0.88 \
+opencv-python-headless==4.11.0.86 \
 scikit-learn scikit-image imageio_ffmpeg pykalman \
 insightface ultralytics \
 onnxruntime-gpu==1.18.0 onnxruntime==1.18.0 \
